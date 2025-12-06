6f427790bb85f3a44892aa46849b153a7b895c32
sweetburble <sweetburble2716@gmail.com>
2025-12-06 11:37:07 +0900

    25.12.06 에러 수정

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,6 @@ function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
-	src(nodeSource).pipe(dest(nodeDestination));
-
-	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
-	const credDestination = path.resolve('dist', 'credentials');
-
-	return src(credSource).pipe(dest(credDestination));
+	// encoding: false 를 줘서 바이너리 깨짐 방지
+	return src(nodeSource, { encoding: false }).pipe(dest(nodeDestination));
 }
